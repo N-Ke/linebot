8dc97b83bccfb715384e1d06df2a1e15cd31a47b
NK <nakashima@MBP.local>
2019-10-03 16:25:54 +0900

    a

--- sample_linebot.php
+++ sample_linebot.php
@@ -70,8 +70,9 @@ $lat = $loccation_test['latitude'];
 $long = $locaation_test['longitude'];
 //位置情報を取得できている。
 			//$json = json_decode(file_get_contents('http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=1d7c45987a45cd65&lat=35.696274&lng=139.74149&format=json&order=4'), true);
+$result = $json['results'];
 			//送信するフロー
-			$bot->add_text_builder($json['result']);
+			$bot->add_text_builder($result);
 
 		}
 
