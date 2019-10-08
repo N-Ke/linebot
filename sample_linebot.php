03de260c846ff3cf56c42cd1112aedc7893562df
NK <nakashima@MBP.local>
2019-10-08 14:06:29 +0900

    a

--- sample_linebot.php
+++ sample_linebot.php
@@ -76,7 +76,7 @@ try {
 			$bot->add_text_builder($json['results']['shop']['0']['name']);*/
 
 
-			$bot->add_text_builder($locaation_test['0']);
+			$bot->add_text_builder($locaation_test['lat']);
 		}
 
 		// 画像メッセージの追加
