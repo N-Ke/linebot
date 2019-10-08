f77b385c96007ef5690a7520123e36e7d72d1bc7
NK <nakashima@MBP.local>
2019-10-08 19:01:34 +0900

    a

--- sample_linebot.php
+++ sample_linebot.php
@@ -68,7 +68,7 @@ $path = '/Users/nakashima/lineboter111/text.txt';
                                  $locaation_test .= $value;
                         }
 
-			$result = file_put_contents($path, $locaation_test, FILE_APPEND);
+			$result = file_put_contents($path, $locaation_test);
 if ($result === 0) {
 $bot->add_text_builder("aaaa");
 } else {
