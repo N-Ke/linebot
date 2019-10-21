4a0a2fa01c7ace1a4e4cb16ea9826ce154dc7aa6
N-Ke <53259583+N-Ke@users.noreply.github.com>
2019-10-21 22:59:53 +0900

    Update linebot.php

--- linebot.php
+++ linebot.php
@@ -61,8 +61,8 @@ class LineBotClass extends LINEBot
 
 	function __construct($default=true)
 	{
-		$accessToken = 'QWMGeklEwfRuDHfBXvyPUkhHQmfwNBSmzlAWSRvJyJ7DAUyubIp9VtIClhljmx1MFBIK7/d8Cy0/m5tfiMOOy25S8Xc6IFH18smRC26ZuBXS5vb+PaMg7YuF0V76fDHm80Lx0pqsAzMkMo5a6KJXnQdB04t89/1O/w1cDnyilFU=';
-		$channelSecret = '794d2ee18b69ac9a07f3c1f6aaceb3b7';
+		$accessToken = 'xxxx';
+		$channelSecret = 'xxxx';
 
 		// アクセストークンでCurlHTTPClientをインスタンス化
 		$http_client = new CurlHTTPClient($accessToken);
