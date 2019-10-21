77473d56dda901d91ff8534c36e5d275de23f15c
N-Ke <53259583+N-Ke@users.noreply.github.com>
2019-10-21 23:10:53 +0900

    Delete define.php

--- define.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('ACCESS_TOKEN','
-QWMGeklEwfRuDHfBXvyPUkhHQmfwNBSmzlAWSRvJyJ7DAUyubIp9VtIClhljmx1MFBIK7/d8Cy0/m5tfiMOOy25S8Xc6IFH18smRC26ZuBXS5vb+PaMg7YuF0V76fDHm80Lx0pqsAzMkMo5a6KJXnQdB04t89/1O/w1cDnyilFU=');  
-define('CHANNEL_SECRET','794d2ee18b69ac9a07f3c1f6aaceb3b7');  
-?>
